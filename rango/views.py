e3873363ad8efd2bd77e1cffed582b558feec6b7
newcoder2202 <avbob2202@gmail.com>
2014-02-22 02:54:58 -0500

    Chapter 3 done.

---
+++ rango/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse('Rango says hello world! <a href="/rango/about">About</a>')
+    
+    
+def about(request):
+    return HttpResponse('Rango says this is the about page. <a href="/rango/">Index</a>')
